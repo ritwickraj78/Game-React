ed9af2f3b1d283a0e4ab752ddfae1e2c783363bf
RitwickRaj78 <ritwickraj78@gmail.com>
2020-04-30 02:40:34 +0530

    Init

---
+++ src/components/figure.js
@@ -0,0 +1,36 @@
+import React,{Component} from 'react';
+
+const images = importAll(require.context('../images', false, /\.(png|jpe?g|svg)$/));
+
+let arr = [];
+
+class Hangman extends Component{
+    componentWillMount(){
+        for(let i=0; i<=6; i++) {
+            arr.push( <img key={i} alt='' src={images[`${i}.png`]} /> );
+        }
+    }
+    showHangman(){
+        let output = arr.slice(0,this.props.incorrect+1);
+        return output;
+    }
+    
+    render(){
+        return (
+            <div className='hangman'>
+              {this.showHangman()}
+            </div>
+          );
+    }    
+};
+
+export default Hangman;
+
+function importAll(r) {
+    let images = {};
+    r.keys().map((item, index) => {
+      images[item.replace('./', '')] = r(item);
+      return null;
+    });
+    return images;
+  }
