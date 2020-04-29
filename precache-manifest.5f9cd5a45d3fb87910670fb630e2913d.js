0cf363215751fd7b494ab8291ba3a656654c5e66
RitwickRaj78 <ritwickraj78@gmail.com>
2020-04-30 03:29:12 +0530

    Updates

---
+++ precache-manifest.5f9cd5a45d3fb87910670fb630e2913d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0528ed9d3d334ef77c8605e45004aae1",
+    "url": "/Game-React/index.html"
+  },
+  {
+    "revision": "faef0a743aab8f38356d",
+    "url": "/Game-React/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "87cfca17aeecf9b5c9d1",
+    "url": "/Game-React/static/js/2.ae1cd4db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Game-React/static/js/2.ae1cd4db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faef0a743aab8f38356d",
+    "url": "/Game-React/static/js/main.4fe4dbc3.chunk.js"
+  },
+  {
+    "revision": "c5696ac3e4eb7dd6be17",
+    "url": "/Game-React/static/js/runtime-main.ee15363d.js"
+  }
+]);
